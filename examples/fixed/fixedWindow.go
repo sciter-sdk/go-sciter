0f5e619dd677956ef9b6f220b01702057c39be3b
oskca <datago@yeah.net>
2015-11-16 11:26:40 +0100

    add fixed example

---
+++ examples/fixed/fixedWindow.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"github.com/oskca/sciter"
+	"github.com/oskca/sciter/window"
+	"log"
+)
+
+func main() {
+	flag.Parse()
+	if flag.NArg() < 1 {
+		log.Fatal("html file needed")
+	}
+	rect := sciter.NewRect(300, 300, 300, 400)
+	// create window
+	w, err := window.New(sciter.DefaultWindowCreaetFlag, rect)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.LoadFile(flag.Arg(0))
+	w.Show()
+	w.Run()
+}
