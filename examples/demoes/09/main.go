4edb8613125bc2e980e14419c39a6c81338fd773
Manish Champaneri <m.champaneri.20@gmail.com>
2020-05-22 18:41:05 +0530

    AddQuitMenu: explanation added to demo 09

--- examples/demoes/09/main.go
+++ examples/demoes/09/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"log"
+
 	"github.com/sciter-sdk/go-sciter"
 	"github.com/sciter-sdk/go-sciter/window"
 )
 
 func main() {
-	win, _ := window.New(sciter.DefaultWindowCreateFlag, sciter.DefaultRect)
+	// Creating application window with default flags and rect size
+	win, err := window.New(sciter.DefaultWindowCreateFlag, sciter.DefaultRect)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// AddQuitMenu function adds Quit menu (in menubar) and support for
+	// CMD+q to terminate / close application on MacOS
+	//
+	// Note : AddQuitMenu() only works for MacOS on other platform is
+	//        does not have any effect.
 	win.AddQuitMenu()
+
 	win.Show()
 	win.Run()
 }
