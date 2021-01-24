385640437eb532b1892f1ba944f8c08df33f205e
pravic <ehysta@gmail.com>
2021-01-24 22:34:07 +0300

    fix: Utf16FromString includes the trailing zero.

--- sciter.go
+++ sciter.go
@@ -400,7 +400,7 @@ func (s *Sciter) Eval(script string) (retval *Value, ok bool) {
 	if err != nil {
 		return nil, false
 	}
-	cscriptLength := C.UINT(len(u16))
+	cscriptLength := C.UINT(len(u16) - 1)
 	cscript := C.LPCWSTR(unsafe.Pointer(&u16[0]))
 	cretval := (*C.SCITER_VALUE)(unsafe.Pointer(retval))
 	// cgo call
@@ -827,7 +827,7 @@ func (e *Element) SetText(text string) error {
 	if err != nil {
 		return err
 	}
-	clength := C.UINT(len(u16))
+	clength := C.UINT(len(u16) - 1)
 	ctext := C.LPCWSTR(unsafe.Pointer(&u16[0]))
 	// cgo call
 	r := C.SciterSetElementText(e.handle, ctext, clength)
@@ -2329,7 +2329,7 @@ func (pdst *Value) ConvertFromString(str string, how ValueStringConvertType) err
 	if err != nil {
 		return err
 	}
-	clen := C.UINT(len(u16))
+	clen := C.UINT(len(u16) - 1)
 	cstr := C.LPCWSTR(unsafe.Pointer(&u16[0]))
 	chow := C.UINT(how)
 	// cgo call
